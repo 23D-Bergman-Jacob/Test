2d4cecba1470951b091cea6b1c8aa367594d52cb
Jacob Bergman <Jacob.Bergman@elev.ga.ntig.se>
2024-11-11 08:50:44 +0100

    upg 5 färdig

--- test/Program.cs
+++ test/Program.cs
@@ -20,10 +20,20 @@
 //     System.Console.WriteLine("Fel användarnamn eller lösenord");
 // }
 // Console.ReadLine();
-int k = 0;
-while (k<32){
-    System.Console.WriteLine("hello world");
-    k++;
-    System.Console.WriteLine(k);
+// int k = 0;
+// while (k<32){
+//     System.Console.WriteLine("hello world");
+//     k++;
+//     System.Console.WriteLine(k);
+// }
+// Console.ReadLine();
+while (true){
+
+    System.Console.WriteLine("Lösenord");
+    string Lösenord = Console.ReadLine();
+    if (Lösenord == "Hejsan"){
+        System.Console.WriteLine("Hejsan på dig");
+        break;
+    } 
 }
 Console.ReadLine();
